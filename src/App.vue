a6cfd2c9033bed11cec9d48abca134cb658e0126
Helena Santana <helena.minond@gmail.com>
2021-04-09 21:37:49 -0400

    proyecto 'props'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h1>Tareas a realizar</h1>
+    <section>
+      <label>Tarea</label>
+      <input v-model="nuevaTarea" type="text" placeholder="Ingresa nueva tarea" size="15" maxlength="40"/>
+      <button @click="AgregarTarea()">Añade</button>
+    </section>
+
+    <Tareas :arrLista="tareas" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Tareas from "./components/Tareas.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      tareas: [
+        { tarea: "Pagar internet" },
+        { tarea: "Comprar pan" },
+        { tarea: "Pagar Netflix" },
+      ],
+      nuevaTarea: "",
+    };
+  },
+  methods: {
+    AgregarTarea() {
+      if (this.nuevaTarea === "") return;
+      this.tareas.push({ tarea: this.nuevaTarea });
+      this.nuevaTarea = "";
+    },
+  },
   components: {
-    HelloWorld,
+    Tareas,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+div {
+  font-family: "Roboto", sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h1,
+h2 {
+  color: gray;
+  margin-top: 2rem;
+}
+
+label,
+li {
+  font-weight: 500;
+}
+
+label {
+  font-size: large;
+}
+
+input,
+button {
+  margin-left: 0.5rem;
+}
+
+button {
+  background-color: white;
+  border: solid 1px gray;
+  border-radius: 5px;
 }
 </style>
---
+++ src/components/Tareas.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Lista</h2>
+    <section v-if="arrLista && arrLista.length == 0">
+      <p>Lista de tareas</p>
+    </section>
+    <section>
+      <ul>
+        <li v-for="nuevaTareaLista in arrLista" :key="nuevaTareaLista">
+          {{ nuevaTareaLista.tarea }}
+        </li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tareas",
+  props: {
+    arrLista: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style></style>
